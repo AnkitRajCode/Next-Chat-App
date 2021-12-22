1cbcd0c971f6390ba843a8072b013c8e92522679
AnkitRajCode <rajankit432@gmail.com>
2021-12-22 18:43:00 +0530

    final push for deploy

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    
+  
+    env: {
+        id:"b405b780-461d-4ba7-bb7c-d2803c0b9b54",
+        key: "ad1e623a-5020-4659-b2f4-4586b0c12fc2"
+    },
+    eslint: {
+      dirs: ['utils'],
+    },
+    
+  };
